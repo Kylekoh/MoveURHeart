b5343b300c3f09e0d082e3bfa35cbd15dd66b415
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2018-11-16 16:45:55 +0900

    modify nav z-index, main-image

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Contact from './Contact';
 import FAQ from './FAQ';
 import Partner from './Partner';
@@ -7,21 +7,35 @@ import OurService from './OurService';
 class Main extends Component {
     render() {
         return (
-            <main className="main-container">
-                <div className="main-introduce">
-                    <div className="introduce-image-container">
-                        <div className="introduce-image">
-                        </div>
+            <Fragment>
+                <div className="main-image">
+                    <div className="main-image-title">
+                        Movement Psychotherapy?
                     </div>
-                    <div className="introduce">무브유어하트 예술심리치유센터는 다양한 심리적 문제 해결
-혹은 더 나은 자신을 만나고자 하는 분들을 위한 심리치유 서비스를 제공합니다.
-특히 우리의 신체와 움직임에 담긴 마음을 발견하고 건강하게 표현하는 데 도움이 될 것입니다</div>
+                    <span className="main-image-sub">
+                        “It is not about the Dance, but about using movement as a tool of communication.<br/> 
+                        It is about connecting movement to psychological process.”
+                    </span>
                 </div>
-                <OurService/>
-                <Partner/>
-                <FAQ/>
-                <Contact/>
-            </main>
+                <main className="main-container">
+                    <div className="main-title">
+                        About Us
+                    </div>
+                    <div className="main-introduce">
+                        <div className="introduce-image-container">
+                            <div className="introduce-image">
+                            </div>
+                        </div>
+                        <span className="introduce">무브유어하트 예술심리치유센터는 다양한 심리적 문제 해결
+    혹은 더 나은 자신을 만나고자 하는 분들을 위한 심리치유 서비스를 제공합니다.
+    특히 우리의 신체와 움직임에 담긴 마음을 발견하고 건강하게 표현하는 데 도움이 될 것입니다</span>
+                    </div>
+                    <OurService/>
+                    <Partner/>
+                    <FAQ/>
+                    <Contact/>
+                </main>
+            </Fragment>
         );
     }
 }
