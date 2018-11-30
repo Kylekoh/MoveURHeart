af3064cbbcdd392250f8e0baa19bc828ee5b24b7
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2018-11-30 17:26:47 +0900

    add scroll effect to navigation bar

--- src/App.js
+++ src/App.js
@@ -6,36 +6,6 @@ import Footer from './components/Footer';
 
 
 class App extends Component {
-
-  // componentDidMount() {
-  //   let mainNavLinks = document.getElementsByClassName("nav-item");
-  //   let mainSections = document.querySelectorAll("main section");
-  //   console.log(mainNavLinks);
-  //   console.log(mainSections);
-  //   let lastId;
-  //   let cur = [];
-    
-  //   window.addEventListener("scroll", () => {
-  //     let fromTop = window.scrollY;
-    
-  //     mainNavLinks.map((link) => {
-  //       let section = document.querySelector(link.hash);
-    
-  //       if (
-  //         section.offsetTop <= fromTop &&
-  //         section.offsetTop + section.offsetHeight > fromTop
-  //       ) {
-  //         link.classList.add("current");
-  //       } else {
-  //         link.classList.remove("current");
-  //       }
-  //     })
-  //     }
-  //    )
-  // }
-
-  handleScroll 
-
   render() {  
     return (
       <Fragment>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,22 +7,26 @@ class Header extends Component {
         this.setWrapperRef = this.setWrapperRef.bind(this);
         this.handleClickOutside = this.handleClickOutside.bind(this);
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
         this.state = {
             isToggledOn : true,
             width: 0,
-            height: 0
+            height: 0,
+            addClass: false,
         }
     }
 
     componentDidMount() {
         document.addEventListener('mousedown', this.handleClickOutside);
         this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);        
+        window.addEventListener('resize', this.updateWindowDimensions);
+        window.addEventListener('scroll', this.handleScroll);    
     }
     
     componentWillUnmount() {
         document.removeEventListener('mousedown', this.handleClickOutside);
         window.removeEventListener('resize', this.updateWindowDimensions);
+        window.removeEventListener('scroll', this.handleScroll);  
     }
 
     updateWindowDimensions() {
@@ -72,26 +76,50 @@ class Header extends Component {
         }    
     }
 
+    handleScroll() {
+        if (window.scrollY > 550) {
+            this.setState({
+              addClass: true
+            })      
+          } else if (window.screenY < 550) {
+            this.setState({
+              addClass: false
+            })
+        }
+    }
+
     render() {
+        let navClass = ['nav-container'];
+        let spanClass = [];
+        let liClass = ['nav-item'];
+        if(this.state.addClass) {
+          navClass.push('colored');
+          spanClass.push('colored-span');
+          liClass.push('colored-li');
+        } else {
+            navClass.slice('colored');
+            spanClass.slice('colored-span');
+            liClass.slice('colored-li');
+        }
         return (
-            <header className="nav-container">
+            <header className={navClass.join(' ')}>
                 <nav className="nav-container-wrapper">
                     <div className="nav-left">
                         <a href="#main-image">
                             <div className="nav-title-wrapper">
                                 <span>Move Your Heart</span>
-                                <span>무브유어하트 예술심리치유센터</span>
+                                <span className={spanClass.join(' ')}>무브유어하트 예술심리치유센터</span>
                             </div>
                         </a>
                         <div className="nav-menu-bar" onClick={this.handleMenuBar}><i className="fas fa-bars"></i></div>
                     </div>
                     <div className="nav-right" ref={this.setWrapperRef}>
                         <ul className="nav-wrapper">
-                            <li className="nav-item"><a href="#main-image" onClick={this.handleMenuBar}>Home</a></li>  
-                            <li className="nav-item"><a href="#main-container" onClick={this.handleMenuBar}>About Us</a></li>
-                            <li className="nav-item"><a href="#main-ourservice" onClick={this.handleMenuBar}>What We Do</a></li>
-                            <li className="nav-item"><a href="#main-service-section" onClick={this.handleMenuBar}>Service</a></li>
-                            <li className="nav-item"><a href="#partner-container" onClick={this.handleMenuBar}>Partner</a></li>
+                            <li className="nav-item"><a href="#main-image" className={liClass.join(' ')} onClick={this.handleMenuBar}>Home</a></li>  
+                            <li className="nav-item"><a href="#main-container" className={liClass.join(' ')} onClick={this.handleMenuBar}>About Us</a></li>
+                            <li className="nav-item"><a href="#main-ourservice" className={liClass.join(' ')} onClick={this.handleMenuBar}>What We Do</a></li>
+                            <li className="nav-item"><a href="#main-service-section" className={liClass.join(' ')} onClick={this.handleMenuBar}>Service</a></li>
+                            <li className="nav-item"><a href="#partner-container" className={liClass.join(' ')} onClick={this.handleMenuBar}>Partner</a></li>
                         </ul>
                     </div>
                 </nav>
